1f0c7cae36012185826d3eb8f6112aaa7c5786d8
Evie Wu <jonwu06@gmail.com>
2022-11-17 16:53:06 -0800

    remove auto-added dependency

--- scuttlebutt/src/main.rs
+++ scuttlebutt/src/main.rs
@@ -1,5 +1,5 @@
 use chrono::{DateTime, Duration, Local, Utc};
-use hmac::{Hmac, digest::typenum::array};
+use hmac::Hmac;
 use jwt::{SignWithKey, VerifyWithKey};
 use poem::{
     listener::TcpListener, web::Data, EndpointExt, Request, Result,
